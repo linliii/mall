88c6cb0265748c85fff1489d55de6cc0a1181e48
ssemahy <20621776@qq.com>
2020-04-20 17:19:07 +0800

    controller 类增加注释

--- mall-server/src/main/java/com/yi10bio/mall/server/controller/ActivityController.java
+++ mall-server/src/main/java/com/yi10bio/mall/server/controller/ActivityController.java
@@ -3,8 +3,9 @@ package com.yi10bio.mall.server.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 活动控制类
+ * 活动服务控制类
  */
 @RestController
 public class ActivityController {
+
 }
--- mall-server/src/main/java/com/yi10bio/mall/server/controller/UserController.java
+++ mall-server/src/main/java/com/yi10bio/mall/server/controller/UserController.java
@@ -3,7 +3,7 @@ package com.yi10bio.mall.server.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 用户控制类
+ * 用户服务控制类
  */
 @RestController
 public class UserController {
