25739edd471a23f6a863be64987a2e8b5c233ec8
ssemahy <20621776@qq.com>
2020-04-22 17:54:01 +0800

    提交数据库，redis 配置文件

---
+++ mall-model/src/main/java/com/yi10bio/mall/model/user/UserAgreement.java
@@ -0,0 +1,85 @@
+package com.yi10bio.mall.model.user;
+
+import java.util.Date;
+
+public class UserAgreement {
+    private Integer id;
+
+    private Integer userId;
+
+    private Integer agreementId;
+
+    private String agreementName;
+
+    private Date agreeDate;
+
+    private String appCode;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getAgreementId() {
+        return agreementId;
+    }
+
+    public void setAgreementId(Integer agreementId) {
+        this.agreementId = agreementId;
+    }
+
+    public String getAgreementName() {
+        return agreementName;
+    }
+
+    public void setAgreementName(String agreementName) {
+        this.agreementName = agreementName == null ? null : agreementName.trim();
+    }
+
+    public Date getAgreeDate() {
+        return agreeDate;
+    }
+
+    public void setAgreeDate(Date agreeDate) {
+        this.agreeDate = agreeDate;
+    }
+
+    public String getAppCode() {
+        return appCode;
+    }
+
+    public void setAppCode(String appCode) {
+        this.appCode = appCode == null ? null : appCode.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
