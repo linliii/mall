39e6b7afe3e453e6a6dd03641df14fc69e8ecddf
renpeng <931322396@qq.com>
2020-05-15 14:36:18 +0800

    redis工具类优化

--- mall-common/src/main/java/com/yishi/mall/config/RedisConfig.java
+++ mall-common/src/main/java/com/yishi/mall/config/RedisConfig.java
@@ -14,23 +14,21 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
-/** redis 配置类 */
+/**
+ * redis 配置类
+ */
 @Configuration
 public class RedisConfig {
+
   @Bean("redisTemplate")
-  public RedisTemplate<Object, Object> redisTemplate(
-      RedisConnectionFactory redisConnectionFactory) {
+  public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
     RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(redisConnectionFactory);
-    Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
-        new Jackson2JsonRedisSerializer<>(Object.class);
+    Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
     ObjectMapper om = new ObjectMapper();
     om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-    om.activateDefaultTyping(
-        LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-    om.registerModule(new ParameterNamesModule())
-        .registerModule(new Jdk8Module())
-        .registerModule(new JavaTimeModule());
+    om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+    om.registerModule(new ParameterNamesModule()).registerModule(new Jdk8Module()).registerModule(new JavaTimeModule());
     om.findAndRegisterModules();
     jackson2JsonRedisSerializer.setObjectMapper(om);
     redisTemplate.setKeySerializer(new GenericToStringSerializer<>(Object.class));
